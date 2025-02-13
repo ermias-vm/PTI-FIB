3164bff478f0f1ba92c0318a61f4a1f5127cffd2
ermias-vm <ermiasvalls.m@gmail.com>
2025-02-14 00:32:04 +0100

    Add car rental web page

---
+++ 1.TomcatJavaServlets/my_webapp/WEB-INF/classes/mypackage/CarRentalList.java
@@ -0,0 +1,84 @@
+package mypackage;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class CarRentalList extends HttpServlet {
+
+  int cont = 0;
+
+  public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    res.setContentType("text/html");
+    PrintWriter out = res.getWriter();
+
+    // Obtenir credencials de l'usuari
+    String user = req.getParameter("userid");
+    String pw = req.getParameter("password");
+
+    if (authenticate(user, pw)) {
+      cont++;
+      out.println("<html><head><title>Rental List</title></head><body>");
+      out.println("<big>Hola Amic " + user + "</big><br>" + cont + " Accesos des de la seva càrrega.<br><br>");
+      out.println("<a href='carrental_home.html'>Home</a><br>");
+      out.println("<h1>Rentals List:</h1>");
+
+      llegirIImprimirRentals(out);
+      out.println("</body></html>");
+    } else {
+      out.println("<html>Usuari o contrasenya incorrectes.</html>");
+    }
+  }
+
+
+  private boolean authenticate(String user, String password) {
+    return "admin".equals(user) && "admin".equals(password);
+  }
+
+  // Funció per llegir els rentals i imprimir-los
+  private void llegirIImprimirRentals(PrintWriter out) {
+    // Ruta del fitxer JSON on es registren els rentals
+    String filePath = "/home/alumne/Documents/PTI/lab/ptiBASE/servlets/apache-tomcat-10.0.10/webapps/my_webapp/rentals.json";
+    File file = new File(filePath);
+
+    if (!file.exists()) {
+      out.println("<p>No s'han trobat rentals.</p>");
+    } else {
+      try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        String line;
+        JSONParser parser = new JSONParser();
+        while ((line = br.readLine()) != null) {
+          try {
+            JSONObject rental = (JSONObject) parser.parse(line);
+            printRental(out, rental);
+          } catch (ParseException pe) {
+            pe.printStackTrace();
+            out.println("Error en el parseig de les dades del rental.<br><br>");
+          }
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        out.println("<p>Error llegint el fitxer de rentals.</p>");
+      }
+    }
+  }
+
+  // Funció per imprimir les dades del lloguer de cotxe
+  private void printRental(PrintWriter out, JSONObject rental) {
+    out.println("co2_rating: " + rental.get("co2_rating") + "<br>");
+    out.println("sub_model_vehicle: " + rental.get("sub_model_vehicle") + "<br>");
+    out.println("dies_lloguer: " + rental.get("dies_lloguer") + "<br>");
+    out.println("num_vehicles: " + rental.get("num_vehicles") + "<br>");
+    out.println("descompte: " + rental.get("descompte") + "%<br><br>");
+  }
+
+  public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    doGet(req, res);
+  }
+}
+ 
---
+++ 1.TomcatJavaServlets/my_webapp/WEB-INF/classes/mypackage/CarRentalNew.java
@@ -0,0 +1,117 @@
+package mypackage;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+
+public class CarRentalNew extends HttpServlet {
+
+  int cont = 0;
+
+  public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    res.setContentType("text/html");
+    PrintWriter out = res.getWriter();
+    cont++;
+
+    String co2Rating = req.getParameter("co2_rating");
+    String subModelVehicle = req.getParameter("sub_model_vehicle");
+    int diesLloguer = Integer.parseInt(req.getParameter("dies_lloguer"));
+    int numVehicles = Integer.parseInt(req.getParameter("num_vehicles"));
+    double descompte = Double.parseDouble(req.getParameter("descompte"));
+
+    // Validar valors numerics obtinguts
+    String errorMsg = validateFields(diesLloguer, numVehicles, descompte);
+    if (!errorMsg.isEmpty()) {
+      out.println("<html><body>");
+      out.println("<h4>Error de validación:</h4>");
+      out.println("<p>" + errorMsg + "</p>");
+      out.println("<a href='carrental_form_new.html'>New rental</a><br>");
+      out.println("<a href='carrental_home.html'>Home</a><br>");
+      out.println("</body></html>");
+      return;
+    }
+
+    // Mapear el valor de CO2 (int -> string)
+    co2Rating = mapCo2Rating(co2Rating);
+
+    // Crear objete JSON amb els parametres obtinguts
+    JSONObject jsonObj = new JSONObject();
+    jsonObj.put("co2_rating", co2Rating);
+    jsonObj.put("sub_model_vehicle", subModelVehicle);
+    jsonObj.put("dies_lloguer", diesLloguer);
+    jsonObj.put("num_vehicles", numVehicles);
+    jsonObj.put("descompte", descompte);
+
+    // Resposta a la peticio de lloguer
+    printHtmlResponse(out, co2Rating, subModelVehicle, diesLloguer, numVehicles, descompte);
+    //guardar les dades del lloguer al JSON
+    writeRentalToFile(jsonObj);
+  }
+
+  private String validateFields(int diesLloguer, int numVehicles, double descompte) {
+    String error = "";
+    if (descompte < 0 || descompte > 100) error += "<br>El percentatge de descompte ha se ser entre 0 y 100. ";
+    if (diesLloguer <= 0) error += "<br>El número de dies de lloguer ha de ser major que 0.";
+    if (numVehicles <= 0) error += "<br>El número de vehicles ha de ser major que 0.";
+    return error;
+  }
+
+  private String mapCo2Rating(String rating) {
+    switch (rating) {
+      case "54":
+        return "Extralow";
+      case "82":
+        return "Medium";
+      case "139":
+        return "High";
+      default:
+        return "Low";
+    }
+  }
+
+  private void printHtmlResponse(PrintWriter out, String co2Rating, String subModelVehicle, int diesLloguer, int numVehicles, double descompte) {
+    out.println(cont + " Accesos desde su carga.<br><br>");
+    out.println("<html><body>");
+    out.println("<h4>Your new rental has been added successfully!</h4><br>");
+    out.println("<h6>co2_rating: " + co2Rating + "</h6>");
+    out.println("<h6>sub_model_vehicle: " + subModelVehicle + "</h6>");
+    out.println("<h6>dies_lloguer: " + diesLloguer + "</h6>");
+    out.println("<h6>num_vehicles: " + numVehicles + "</h6>");
+    out.println("<h6>descompte: " + descompte + " %</h6><br>");
+    out.println("<a href='carrental_form_new.html'>New rental</a><br>");
+    out.println("<a href='carrental_home.html'>Home</a><br>");
+    out.println("</body></html>");
+  }
+  
+  private void writeRentalToFile(JSONObject jsonObj) {
+    String filePath = "/home/alumne/Documents/PTI/lab/ptiBASE/servlets/apache-tomcat-10.0.10/webapps/my_webapp/rentals.json";
+    File file = new File(filePath);
+
+    if (!file.exists()) { // es crea arxiu si no existeix
+      try {
+        file.createNewFile();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    //  afegir una linea amb les dades obtingudes al JSON 
+    try (FileWriter fileWriter = new FileWriter(file, true);
+         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+      bufferedWriter.write(jsonObj.toJSONString() + "\n");
+      bufferedWriter.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+    doGet(req, res);
+  }
+}
